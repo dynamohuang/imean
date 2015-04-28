4da02f5e8e4f974654a2ae2cb3dfe7b6b23408c1
dev <hdz@localhost.localdomain>
2015-04-27 19:52:33 -0700

    init

---
+++ config/express.js
@@ -0,0 +1,6 @@
+var express = require('express');
+module.exports = function() {
+ var app = express();
+ require('../app/routes/index.server.routes.js')(app);
+ return app;
+};
---
+++ server.js
@@ -0,0 +1,6 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var express = require('./config/express');
+var app = express();
+app.listen(3000);
+module.exports = app;
+console.log('imean is running at http://localhost:3000/');
