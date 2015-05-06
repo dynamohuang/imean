aeded2c13c2d533e042d9478c8c7153375e499bf
dev <huangdinzhi1993@gmail.com>
2015-05-06 06:56:47 -0700

    add user login function in cms

--- app/routes/cms.server.routes.js
+++ app/routes/cms.server.routes.js
@@ -1,4 +1,15 @@
 module.exports = function(app){
         var index = require("../controllers/cms/index.server.controller");
+	var users = require('../controllers/cms/users.server.controller'),
+ 	passport = require('passport');
         app.get('/admin',index.render);
+        app.route('/admin/signin')
+	.get(users.renderSignin)
+ 	.post(passport.authenticate('local', {
+ 	successRedirect: '/admin',
+ 	failureRedirect: '/admin/signin',
+ 	failureFlash: true
+ 	}));
+ 	app.get('/admin/signout', users.signout);
+        
 }
